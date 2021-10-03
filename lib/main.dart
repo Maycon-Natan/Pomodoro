c82e6c5aae160d6d5c99bf3fdf37a8358accfaee
Maycon-Natan <mayconnatan7@gmail.com>
2021-10-03 16:07:50 -0300

    inc e dec button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:relogiomobx/pages/Pomodoro.dart';
+import 'package:provider/provider.dart';
+import 'package:relogiomobx/store/pomodoro.store.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +11,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        Provider<PomodoroStore>(
+          create: (_) => PomodoroStore(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Pomodoro(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: Pomodoro(),
     );
   }
 }
